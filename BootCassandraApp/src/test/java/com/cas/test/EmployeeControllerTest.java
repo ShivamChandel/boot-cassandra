d1fa3d2d18e90d114d1af3da54d3d0a9aa6bfac6
Shivam_Chandel <Shivam_Chandel@CHDSEZ297703D.ad.infosys.com>
2019-08-01 17:57:21 +0530

    code refined for test class

--- BootCassandraApp/src/test/java/com/cas/test/EmployeeControllerTest.java
+++ BootCassandraApp/src/test/java/com/cas/test/EmployeeControllerTest.java
@@ -6,7 +6,8 @@
 
 package com.cas.test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,38 +17,59 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import com.cas.config.CassandraApplication;
 import com.cas.controller.EmployeeController;
 import com.cas.entity.Employee;
 import com.cas.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { CassandraApplication.class })
 @WebMvcTest(EmployeeController.class)
 public class EmployeeControllerTest {
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	MvcResult mvcResult = null;
+
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext context;
+	// to mock dependency,your rest contoller might need to execute
 	@MockBean
 	private EmployeeService employeeService;
 
-	Employee empMockObj = new Employee(0, "mockName", "mockUnit", 0);
+	// setup method to intialise mock mvc object to be used later
+	@Before
+	public void initialise() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 
 	@Test
 	public void getAllEmployeesTest() throws Exception {
-
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/cas/getAllEmployees")
+		RequestBuilder requestBuilderGet = MockMvcRequestBuilders.get("/cas/getAllEmployees")
 				.accept(MediaType.APPLICATION_JSON);
-		Integer status = mockMvc.perform(requestBuilder).andReturn().getResponse().getStatus();
-		System.out.println("The status of the GET test case : " + status);
-		// assertEquals("", 200, status);
+		mvcResult = mockMvc.perform(requestBuilderGet).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		System.out.println("The status of the GET test case : " + mvcResult.getResponse().getStatus());
+		Assert.assertTrue("GET test case passed", mvcResult.getResponse().getStatus() == 200);
 	}
 
 	@Test
 	public void createEmployeeTest() throws Exception {
 
+		Employee emp = new Employee(90, "mockName", "mockUnit", 0);
+		String responseBody = objectMapper.writeValueAsString(emp);
+		RequestBuilder requestBuilderPost = MockMvcRequestBuilders.post("/cas/createEmployee").content(responseBody)
+				.accept(MediaType.APPLICATION_JSON);
+		mvcResult = mockMvc.perform(requestBuilderPost).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		System.out.println("The status of the POST test case : " + mvcResult.getResponse().getStatus());
+		Assert.assertTrue("POST test case passed", mvcResult.getResponse().getStatus() == 201);
 	}
 
 	@Test
